3acd000351335eebd67f55e79a78d8374c1b4919
DESKTOP-J46C4PB\Empty <cipuksiblol@gmail.com>
2026-02-22 13:58:10 +0100

    3

--- scan.py
+++ scan.py
@@ -3,9 +3,39 @@ import json
 from datetime import datetime
 
 import ctypes
+from ctypes import wintypes
+import winreg
 
+import time
+#---------------------------------------------
+def get_unc_path(drive_letter):
+    drive_letter = drive_letter.strip().rstrip('\\')
+    mpr = ctypes.WinDLL('mpr')
+    
+    buffer_len = wintypes.DWORD(512)
+    buffer = ctypes.create_unicode_buffer(buffer_len.value)
+    
+    result = mpr.WNetGetConnectionW(drive_letter, buffer, ctypes.byref(buffer_len))
+    if result == 0:
+        return buffer.value
+    return None
+#---------------------------------------------
+def get_local_label_from_reg(drive_letter):
+    unc_path = get_unc_path(drive_letter)
+    if not unc_path:
+        return None
 
-#todo network drive labels
+    registry_key_name = "##" + unc_path.lstrip('\\').replace('\\', '#')
+    
+    full_key_path = rf"Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\{registry_key_name}"
+    
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, full_key_path) as key:
+            label, _ = winreg.QueryValueEx(key, "_LabelFromReg")
+            return label
+    except FileNotFoundError:
+        return None
+#---------------------------------------------
 def get_drive_label(drive_letter):
     kernel32 = ctypes.windll.kernel32
     volumeNameBuffer = ctypes.create_unicode_buffer(1024)
@@ -26,9 +56,15 @@ def get_drive_label(drive_letter):
     )
     return volumeNameBuffer.value
 
-
+#---------------------------------------------
 def scan_drive(drive_path):
-    drive_label = get_drive_label(drive_path)
+    label = get_local_label_from_reg(drive_path)
+    if label:
+        drive_label = label
+    else:
+        drive_label = get_drive_label(drive_path)
+
+
     print(drive_path+" - "+drive_label)
 
     result = {
@@ -75,12 +111,15 @@ def scan_drive(drive_path):
         f["files"].sort()
 
     return result
-
+#---------------------------------------------
 
 drives = ["C:\\","D:\\", "E:\\", "F:\\", "G:\\", "H:\\", "M:\\", "N:\\", "S:\\", "T:\\", "U:\\", "Z:\\"]
 
 data = []
 
+time_start = time.perf_counter()
+time_drive_start = time_start
+#---------------------------------------------
 for drive in drives:
     try:
         data.append(scan_drive(drive))
@@ -89,10 +128,15 @@ for drive in drives:
             "drive": drive,
             "error": str(e)
         })
+    print(f"{(time.perf_counter() - time_drive_start) * 1000:.2f} ms")
+    time_drive_start = time.perf_counter()
 
 with open("drives_data.js", "w", encoding="utf-8") as f:
     f.write("const jsonData = ")
     json.dump(data, f, indent=2)
     f.write(";")
-
-print("Done.")
+#---------------------------------------------
+print("done")
+time_end = time.perf_counter()
+elapsed_ms = (time_end - time_start) * 1000
+print(f"{elapsed_ms:.2f} ms")
